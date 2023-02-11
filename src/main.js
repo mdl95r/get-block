0e3f676cabd12ff56c4bc31564a7bb60a413a25e
Artyom <awesomemdl95@gmail.com>
2023-02-11 18:19:23 +0300

    other

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store";
-import components from "./ui/index";
-import vClickOutside from "click-outside-vue3";
+import directives from "@/directives";
 
 const app = createApp(App);
 
-components.forEach((component) => {
-  app.component(component.name, component);
-});
+directives.forEach((directive) => app.directive(directive.name, directive));
 
-app.use(vClickOutside);
-
-app.use(store).mount("#app");
+app.mount("#app");
--- src/ui/index.js
+++
@@ -1,5 +0,0 @@
-import GbButton from "./gb-button/GbButton.vue";
-import GbInput from "./gb-input/GbInput.vue";
-import GbSelect from "./gb-select/GbSelect.vue";
-
-export default [GbButton, GbInput, GbSelect];
